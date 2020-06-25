e08a337d949c454d3ffb5672c19e335f9a3dd001
SushmitaSB <sushmitabiswas8888@gmail.com>
2020-06-26 01:42:20 +0530

    3rd commit

--- app/src/main/java/com/example/tailwebsapp/view/IntroActivity.java
+++ app/src/main/java/com/example/tailwebsapp/view/IntroActivity.java
@@ -45,6 +45,7 @@ public class IntroActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
